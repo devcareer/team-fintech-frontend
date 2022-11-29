ff73ec41b950d639549ed47ecbcd20e8ead7726e
kingjamesx <talk2james.uj@gmail.com>
2022-11-29 16:47:23 +0100

    fix: button component

--- src/components/General/Button/Button.js
+++ src/components/General/Button/Button.js
@@ -1,13 +1,18 @@
 // The button component accepts some props
 //  the size and the type of button
+// It now accepts a margin top prop too mt
 //  to use <Button size='' type=''>button name</Button>
 //  If you dont specify it uses the default
 // currently there are only 2 button size the default and lg
+// mt-${mt} p-2.5 bg-primary-300 w-full  rounded-lg whitespace-nowrap text-white text-base hover:bg-primary-200
+//       lg:text-lg py-4 px-14;
 import PropTypes from 'prop-types';
 import React from 'react';
-const Button = ({ children, size, type }) => {
+const Button = ({ children, type }) => {
   return (
-    <button type={type} className={`btn btn-${size}`}>
+    <button
+      type={type}
+      className="p-2.5 bg-primary-300 w-full  rounded-lg whitespace-nowrap text-white text-base hover:bg-primary-200 lg:h-[60px]">
       {children}
     </button>
   );
@@ -15,7 +20,6 @@ const Button = ({ children, size, type }) => {
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  size: PropTypes.string,
   type: PropTypes.string
 };
 export default Button;
