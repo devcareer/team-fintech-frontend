8820af756441772743ef90406eb08cb52a504f23
kingjamesx <talk2james.uj@gmail.com>
2022-11-29 19:26:49 +0100

    refactor: update component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import Button from './components/General/Button/Button';
-import Input from './components/General/Input/Input';
-import InputPassword from './components/General/Input/InputPassword';
-
 function App() {
   return (
-    <div className="flex flex-col gap-10 p-4 mx-auto lg:w-[632px]">
+    <div>
       <h1>Welcome to the fintech App</h1>
-      <div className="flex flex-col gap-8 mx-auto ">
-        <Input />
-        <Input />
-        <Input />
-        <InputPassword />
-        <Button>Hello world</Button>
-      </div>
     </div>
   );
 }
--- src/components/General/Input/InputPassword.js
+++ src/components/General/Input/InputPassword.js
@@ -1,6 +1,5 @@
-// This component accepts a labeltext prop to help you specify the
-// label you want to use <InputPassword label='your label'/>
-// if you dont specify a label it uses the default
+// This component accepts a labeltext and placeholder prop to help you specify the
+// label you want to use and placeholder <InputPassword label='your label'/>
 import PropTypes from 'prop-types';
 import React from 'react';
 import eye from '../../../assets/icons/eye-icon.svg';
@@ -12,9 +11,9 @@ const InputPassword = ({ labeltext, placeholder }) => {
       <input
         type="password"
         placeholder={placeholder}
-        className={`w-full  appearance-none border border-outline rounded-lg  py-3 px-4  text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-text-3 placeholder:text-base lg:w-[400px]  `}
+        className="w-full  appearance-none border border-outline rounded-lg  py-3 px-4  text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder-text-3 placeholder:text-base lg:w-[400px] "
       />
-      <button className={`absolute left-[280px] bottom-4 lg:left-[358px] `}>
+      <button className="absolute left-[280px] bottom-4 lg:left-[358px]">
         <img src={eye} alt="eye-icon" />
       </button>
     </div>
