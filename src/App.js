855ddff72e069acf22a01e6a165b1b1b841beaf5
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-12-02 09:48:31 +0100

    fix: component styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import InputPhone from './components/General/Input/InputPhone';
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the fintech App</h1>
+      <InputPhone />
     </div>
   );
 }
