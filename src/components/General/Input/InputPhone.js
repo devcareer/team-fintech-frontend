4e4ad36a35a0a33e361283443c3bad44ae4ee21b
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-12-02 07:55:55 +0100

    fix: phone input component

---
+++ src/components/General/Input/InputPhone.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import { useState } from 'react';
+import './InputPhone.css';
+
+const InputPhone = () => {
+  const [value, setValue] = useState();
+  return (
+    <div className="flex justify-center">
+      <PhoneInput
+        placeholder="Enter phone number"
+        defaultCountry="RU"
+        value={value}
+        onChange={setValue}
+        // PhoneInputCountryFlag-height {
+        //     height: 3;
+        // }
+        className="border border-outline py-3 px-4 flex w-96 rounded-lg"
+      />
+    </div>
+  );
+};
+
+export default InputPhone;
