1cc2a93726fe436e1184d9c10e7d46850759fc5d
kingjamesx <talk2james.uj@gmail.com>
2022-12-05 13:40:15 +0100

    style: add styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import InputPhone from './components/General/Input/InputPhone';
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the fintech App</h1>
+      <InputPhone />
     </div>
   );
 }
